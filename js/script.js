b81aff7a1e91d94523b9adb4aed82eaf1e7a6f39
besailazi <126988974+besailazi@users.noreply.github.com>
2024-02-08 11:14:35 +0100

    final commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const jokeContainer = document.querySelector('.joke-container');
 
 const fetchJoke = async() => {
 	try{
-	const response = await fetch('https://ap.chucknorris.io/jokes/random');
+	const response = await fetch('https://api.chucknorris.io/jokes/random');
 	const data = await response.json();
 	return data.value
 	} catch (error){
